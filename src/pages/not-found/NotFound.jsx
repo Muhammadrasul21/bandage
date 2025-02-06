cd74f2da4d50cca15956dde1d7ccde324e3ae4e2
Muhammadrasul21 <theboy6878@gmail.com>
2025-02-06 20:29:27 +0500

    ficed notFound page

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>notFound</div>;
+  return <div className="container flex items-center justify-center flex-col gap-8">
+    <p className="font-bold text-5xl mt-6">404</p>
+    <p className="font-bold text-5xl">Page Not Found</p>
+  </div>;
 };
 
 export default NotFound;
